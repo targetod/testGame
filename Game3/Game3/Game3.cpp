30c4f26ca5aaeb257d9564efc513344373b41a64
targetod <dragon_sasha@meta.ua>
2015-03-13 21:17:43 +0200

    commit2

--- Game3/Game3/Game3.cpp
+++ Game3/Game3/Game3.cpp
@@ -8,12 +8,39 @@ constexpr unsigned int wndWidth{ 800 }, wndHeight{ 600 };
 class Ball
 {
 public:
-	static const sf::Color def
+	static const sf::Color defColor;
+	static constexpr float defRadius{ 10.f };
+	static constexpr float defVelocity{ 1.f };
+
+	sf::CircleShape shape;
+
+	sf::Vector2f velocity{ -defVelocity, -defVelocity };
+
+	Ball(float mX, float mY)
+	{
+		shape.setPosition(mX, mY);
+		shape.setRadius(defRadius);
+		shape.setFillColor(defColor);
+		shape.setOrigin(defRadius, defRadius);
+	}
+
+	void update()
+	{
+		shape.move(velocity);
+	}
+	void draw(sf::RenderWindow& mTarget)
+	{
+		mTarget.draw(shape);
+	}
 };
+
+const sf::Color Ball::defColor{ sf::Color::Red };
 
 int main(int argc, char *argv[])
 {
-	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 1" };
+	Ball ball{ wndWidth / 2., wndHeight / 2.f };
+
+	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 2" };
 	window.setFramerateLimit(60);
 
 	while (true)
@@ -21,6 +48,11 @@ int main(int argc, char *argv[])
 		window.clear(sf::Color::Black);
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
 			break;
+
+		ball.update();
+		ball.draw(window);
+
+
 		window.display();
 	}
 	
